f143ddfe6568538b60b637b4913fa7a7155ba2fb
AlmaSotelo <angieandme17@gmail.com>
2025-03-14 22:35:09 -0700

    Final push for grading. Includes Exceeds the core requirements.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.Metrics;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
@@ -11,74 +12,102 @@ class Program
         Console.WriteLine("Hello World! This is the Journal Project.");
         string userOption;
         Journal myJournal = new Journal();
-        do
+        int counter = 0;
+        string exiting = "";
+        //Exceeds core 1(a): outter loop to confirm exiting.
+        while (exiting != "y")
         {
-            Console.WriteLine("Please select one of the following choices: ");
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
-            Console.WriteLine("5. Quit");
-            Console.Write("What would you like to do ? ");
-            userOption = Console.ReadLine();
-
-            switch (userOption)
+            do
             {
-                case "1":
+                Console.WriteLine("Please select one of the following choices: ");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display");
+                Console.WriteLine("3. Load");
+                Console.WriteLine("4. Save");
+                Console.WriteLine("5. Quit");
+                Console.Write("What would you like to do ? ");
+                userOption = Console.ReadLine();
 
-                    //create an instance of Entry class.
-                    Entry entry = new Entry();
-                    entry._date = entry.GetDate();
+                if (userOption is "1" or "2" or "3" or "4")
+                {
+                    counter++;
+                    switch (userOption)
+                    {
+                        case "1":
 
-                    entry._rnmprompt = entry.GetPrompt();
-                    Console.WriteLine(entry._rnmprompt);
+                            //create an instance of Entry class.
+                            Entry entry = new Entry();
+                            entry._date = entry.GetDate();
 
-                    //let the user enter the writing
-                    Console.Write(">");
-                    entry._message = Console.ReadLine();
+                            entry._rnmprompt = entry.GetPrompt();
+                            Console.WriteLine(entry._rnmprompt);
 
-                    myJournal.AddEntry(entry);
-                    
-                    break;
+                            //let the user enter the writing
+                            Console.Write(">");
+                            entry._message = Console.ReadLine();
 
-                case "2":
-                    myJournal.Display();
+                            myJournal.AddEntry(entry);
 
-                    break;
+                            break;
 
-                case "3":                    
-                    myJournal.FileLoader();
+                        case "2":
+                            myJournal.Display();
 
-                    break;
+                            break;
 
-                case "4":
-                    Console.WriteLine("Enter the name of the file: ");
-                    string filename = Console.ReadLine();
-                    myJournal.FileSaver(filename);
-                     
-                    break;
+                        case "3":
+                            myJournal.FileLoader();
 
-                case "5":
+                            break;
 
-                    break;
+                        case "4":
+                            Console.WriteLine("Enter the name of the file: ");
+                            string filename = Console.ReadLine();
+                            myJournal.FileSaver(filename);
 
+                            break;
+                    }
+                }
+                //Exceeds core 2: evaluate an acceptable input
+                else if (userOption != "5")
+                {
+                    Console.WriteLine("Enter a valid answer (1,2,3,4 or 5) ");
+                }
+
+            } while (userOption != "5");
+
+            //Exceeds core 1(b): if the user did not enter any options 1,2,3, or 4, but jumps to option 5 right after accesing the program, then, is asked to connfirm his exit.
+            if (counter == 0)
+            {
+                Console.Write("Are you sure you want to leave? y/n? ");
+                exiting = Console.ReadLine();
+                if (exiting == "y")
+                {
+                    Console.WriteLine("By now");
+                }
             }
-
-
-        } while (userOption != "5");
-
-
-
-
-
-
-
-
-
-
-
+            
+            //Exceeds core  1(c): the user have no confirmation for exiting, but a nice good by after actually using the program
+            else{                
+                Console.WriteLine("Thank you for visiting. See you soon.");
+                exiting = "y";
+            }
+        }
 
         
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
